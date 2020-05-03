519d7cca23f89cf93cc6a50ad76cc0a8e6c23ee1
Opeyemi Adeyeye <simeondominiq@gmail.com>
2020-05-03 08:22:05 +0100

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import store from './store';
+import App from './App.vue';
+import { router } from './router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    router,
+    store,
+    render: (h) => h(App),
+}).$mount('#app');
